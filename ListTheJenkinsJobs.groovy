fc63bcf0610c9db7fa1a91621b1e5d61a350f08f
venkatasykam <venkatasykam@yahoo.com>
2018-06-04 15:38:02 +0530

    Update ListTheJenkinsJobs.groovy

--- ListTheJenkinsJobs.groovy
+++ ListTheJenkinsJobs.groovy
@@ -1,4 +1,12 @@
 import jenkins.model.*
+import hudson.model.*
+import hudson.AbortException
+import hudson.console.HyperlinkNote
+import java.util.concurrent.CancellationException
+ 
+// Retrieve parameters of the current build
+def jobPattern = build.buildVariableResolver.resolve("jobPattern")
+println "jobPattern=$jobPattern"
 
 //def jobPattern = params.jobPattern
 
